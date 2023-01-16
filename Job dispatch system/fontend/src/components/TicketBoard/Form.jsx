0423b92b94f675cc02fedb61f6cd29171194f722
kanlo456 <kanson456@gmail.com>
2023-01-17 05:01:49 +0800

    x

---
+++ Job dispatch system/fontend/src/components/TicketBoard/Form.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const TicketForm = () => {
+    return (
+        <Fragment>
+            <form>
+                
+            </form>
+        </Fragment>
+    )
+
+};
+export default TicketForm
---
+++ Job dispatch system/fontend/src/components/TicketBoard/TiceketBoard.jsx
@@ -0,0 +1,8 @@
+const TicketBoard = () => {
+  return (
+    <div className="fit-content">
+      <h1>Tickets</h1>
+    </div>
+  );
+};
+export default TicketBoard;
