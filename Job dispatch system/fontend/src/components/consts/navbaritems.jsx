fca2bfaa0ba30f511d9cab471e289367716bbdd7
kanlo456 <kanson456@gmail.com>
2023-01-17 23:43:33 +0800

    x

--- Job dispatch system/fontend/src/components/consts/navbaritems.jsx
+++ Job dispatch system/fontend/src/components/consts/navbaritems.jsx
@@ -1,8 +1,17 @@
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
+
 export const navbaritems = [
     {
         id:0,
-        icon:'',
-        label:'Authentication',
+        icon:<DashboardIcon/>,
+        label:'Dashboard',
         route:'rotue'
+    }
+    ,{
+        id:1,
+        icon:<ConfirmationNumberIcon/>,
+        label:'Ticket Board',
+        route:'route'
     }
 ]
