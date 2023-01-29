4b044c60e18ab1e62baca764911a28dbb08290b4
kanlo456 <kanson456@gmail.com>
2023-01-29 23:33:00 +0800

    update

---
+++ Job dispatch system/frontend/src/chatbot/Facebook.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Facebook.css";
+
+const Facebook = () => {
+  return (
+    <a href="https://www.facebook.com/profile.php?id=100064909925328">
+      <i class="fa fa-facebook my-float3"></i>
+    </a>
+  );
+};
+
+export default Facebook;
