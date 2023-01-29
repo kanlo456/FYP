c3217dc8eee0f050ba04edf4a68b5b5fd01920fd
kanlo456 <kanson456@gmail.com>
2023-01-29 23:43:37 +0800

    update

--- Job dispatch system/backend/models/ticketModel(Oscar version).js
+++ Job dispatch system/backend/models/ticketModel(Oscar version).js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require("validator");
 
-
 const Schema = mongoose.Schema
 
 const ticketSchema = new Schema({
--- Job dispatch system/frontend/src/pages/SignUpPage.js
+++ Job dispatch system/frontend/src/pages/SignUpPage.js
@@ -3,8 +3,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -41,13 +39,6 @@ export default function SignUp() {
   const { signup, error, isLoading } = useSignup();
   const handleSubmit = async (values) => {
     await signup(
-      values.userName,
-      values.password,
-      values.emailAddress,
-      values.firstName,
-      values.lastName,
-    );
-    console.log(
       values.userName,
       values.password,
       values.emailAddress,
