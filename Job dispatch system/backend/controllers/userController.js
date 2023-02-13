0efc2155c450b8e1556e3ba1b8fc25d8d6380436
ACFWWU <101387130+ACFWWU@users.noreply.github.com>
2023-02-14 03:12:03 +0800

    Add files via upload

--- Job dispatch system/backend/controllers/userController.js
+++ Job dispatch system/backend/controllers/userController.js
@@ -25,8 +25,8 @@ const loginUser = async(req, res) => {
     // res.json({ message: 'login user' })
 };
 
-//signup user
-const signUser = async(req, res) => {
+//cretae staff account
+const createStaff = async(req, res) => {
     const { username, password, email, firstname, lastname, role } = req.body
     try {
         const user = await User.signup(username, password, email, firstname, lastname, role)
@@ -41,4 +41,20 @@ const signUser = async(req, res) => {
     }
 };
 
-module.exports = { loginUser, signUser }
+//signup user
+const signUser = async(req, res) => {
+    const { username, password, email, firstname, lastname} = req.body
+    try {
+        const user = await User.signup(username, password, email, firstname, lastname)
+
+        //create a token
+        const token = createToken(user._id)
+
+        // res.status(200).json({email,user})
+        res.status(200).json({ username, token })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+};
+
+module.exports = { loginUser,createStaff, signUser }
