76a0fca85365b1adcfcbc0e58415fe5c34a1fe4c
ACFWWU <101387130+ACFWWU@users.noreply.github.com>
2023-02-14 03:12:28 +0800

    Add files via upload

--- Job dispatch system/backend/routes/user.js
+++ Job dispatch system/backend/routes/user.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
 //controller
-const {loginUser,signUser} = require('../controllers/userController');
+const {loginUser,createStaff,signUser} = require('../controllers/userController');
 
 const router = express.Router();
 
 //login route
 router.post('/login',loginUser)
 
+//cretae staff account
+router.post('/cs',createStaff)
+
 //signup route
 router.post('/signup',signUser)
+
+
 
 module.exports = router;
