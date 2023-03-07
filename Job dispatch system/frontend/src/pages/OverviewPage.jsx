d4309291c22c27b071c30df15aebf2e153b48270
kanlo <kanson456@gmail.com>
2023-03-07 09:51:58 +0800

    no need

---
+++ Job dispatch system/frontend/src/pages/OverviewPage.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { Card } from "@mui/material";
+
+const OverviewPage = () => {
+  return (
+    <Fragment>
+      <Card> test 123</Card>
+    </Fragment>
+  );
+};
+
+export default OverviewPage;
