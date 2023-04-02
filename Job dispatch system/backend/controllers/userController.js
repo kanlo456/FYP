d58c8ecdd48b4450e30c7692ac28868b38fd7420
ACFWWU <101387130+ACFWWU@users.noreply.github.com>
2023-04-02 18:56:08 +0800

    Update userController.js

--- Job dispatch system/backend/controllers/userController.js
+++ Job dispatch system/backend/controllers/userController.js
@@ -12,12 +12,13 @@ const loginUser = async(req, res) => {
 
     try {
         const user = await User.login(username, password)
+        const role = await User.distinct( "role", {username:username} )
 
         //create a token
         const token = createToken(user._id)
 
         // res.status(200).json({email,user})
-        res.status(200).json({ username, token })
+        res.status(200).json({ username,role,token })
 
     } catch (error) {
         res.status(400).json({ error: error.message })
