ee216d7cfc8b63ea5dc16d38dcad8db9ea10bebc
ACFWWU <101387130+ACFWWU@users.noreply.github.com>
2023-04-23 22:35:35 +0800

    Update user.js

--- Job dispatch system/backend/routes/user.js
+++ Job dispatch system/backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 //controller
-const {loginUser,createStaff,signUser,getUserInfo,getHandleTicket} = require('../controllers/userController');
+const {loginUser,createStaff,signUser,getUserInfo,getHandleTicket,getCustomerTickets} = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -19,5 +19,8 @@ router.get('/info',getUserInfo)
 
 //get handle ticket by username
 router.get('/handle',getHandleTicket)
+
+//get customer ticket
+router.get('/cstt',getCustomerTickets)
 
 module.exports = router;
