42010fe84e4224a4a7bbf51039af89b981bab65e
SunnyKwok0415 <109259987+SunnyKwok0415@users.noreply.github.com>
2023-04-24 16:10:16 +0800

    Update ticketController.js

--- Job dispatch system/backend/controllers/ticketController.js
+++ Job dispatch system/backend/controllers/ticketController.js
@@ -172,7 +172,7 @@ const updateTicket =  async(req,res)=>{
         return res.status(404).json({error:'No such ticket, can not update the ticket'})
     }
     if(req.body.status === "Solved"){
-        sendEmail();
+        //sendEmail();
         // return resolve({message:"The ticket complete help us to improve our service"})
     }  
     res.status(200).json(ticket)
